dc4be83d7c70eb22f69ae9bd92dfd66ac19cb27f
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2021-12-13 12:49:38 +0900

    Add custom document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,32 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import nprogress from "nprogress";
+import "nprogress/nprogress.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "utils/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  if (process.browser) {
+    nprogress.start();
+  }
+
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeComplete", (url) => {
+      console.log(url);
+      nprogress.done();
+      gtag.pageView(url);
+    });
+    nprogress.done();
+    return () => {
+      router.events.off("routeChangeComplete", (url) => {
+        gtag.pageView(url);
+      });
+    };
+  }, [router.events]);
+
   return (
     <ChakraProvider>
       <Component {...pageProps} />
